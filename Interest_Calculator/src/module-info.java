d48d0eecee8eb419c59c39ae064cf62b7e8a241b
Aarush <aarush1998@gmail.com>
2026-02-27 10:39:55 +0530

    Add files via upload

---
+++ Interest_Calculator/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Interest_Calculator {
+}
---
+++ Interest_Calculator/src/spring/Account.java
@@ -0,0 +1,8 @@
+package spring;
+
+abstract class Account {
+    double interestRate;
+    double amount;
+
+    abstract double calculateInterest() throws InvalidInputException;
+}
