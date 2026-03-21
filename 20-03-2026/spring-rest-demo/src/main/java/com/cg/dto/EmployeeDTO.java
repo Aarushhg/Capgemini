d68a66eabbec7fc29c87635a6f68213867b4fb90
Aarush <aarush1998@gmail.com>
2026-03-21 14:51:13 +0530

    Add files via upload

---
+++ 20-03-2026/spring-rest-demo/src/main/java/com/cg/dto/EmployeeDTO.java
@@ -0,0 +1,68 @@
+package com.cg.dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class EmployeeDTO {
+
+	private Integer employeeId;
+	@NotNull(message = "name is required")
+	@NotBlank(message = "name should not be blank")
+	private String fullName;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+	private LocalDate dateOfBirth;
+	@NotNull(message = "salary is requored ")
+	@Min(value = 500000)
+	private Double salary;
+
+
+
+	public EmployeeDTO() {
+		super();
+	}
+
+	public EmployeeDTO(Integer id, String fullName, LocalDate dateOfBirth, Double salary) {
+		this.employeeId = id;
+		this.fullName = fullName;
+		this.dateOfBirth = dateOfBirth;
+		this.salary = salary;
+	}
+
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+}
---
+++ 20-03-2026/spring-rest-demo/src/main/java/com/cg/entity/Employee.java
@@ -0,0 +1,65 @@
+package com.cg.entity;
+
+import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement(localName = "Employee")
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer empid;
+	private String name;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+	private LocalDate dob;
+	private Double salary;
+
+
+
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String name, LocalDate dob, Double salary) {
+		super();
+
+		this.name = name;
+		this.dob = dob;
+		this.salary = salary;
+	}
+	public Integer getEmpid() {
+		return empid;
+	}
+	public void setEmpid(Integer empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+
+
+
+}
